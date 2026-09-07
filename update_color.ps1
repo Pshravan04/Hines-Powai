054afddf68aa9b81f5973ec876cd74690a9454c4
Pshravan04 <shravanphutanr@gmail.com>
2026-09-07 11:22:17 +0530

    refactor: remove obsolete automation scripts and utility files from the hiens site directory

--- update_color.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path "d:\TGM - sites\hiens\*.html"
-foreach ($file in $files) {
-    $content = Get-Content -Path $file.FullName -Raw -Encoding UTF8
-    $newContent = $content.Replace('#100906', '#AC0523')
-    if ($content -ne $newContent) {
-        Set-Content -Path $file.FullName -Value $newContent -Encoding UTF8
-        Write-Host "Updated $($file.Name)"
-    }
-}
